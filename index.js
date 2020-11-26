56272faa417e630842a433207eec3ab0394e261d
oliholeh <oliholeh1988@gmail.com>
2020-11-26 13:58:07 +0200

    Connect all libraries/// Create app, io, server and start up all//

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
